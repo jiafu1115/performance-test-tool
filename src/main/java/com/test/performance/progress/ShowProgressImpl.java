0bbda96530453c78ac01becce81426a765205092
stroller <fujian1115@gmail.com>
2018-03-18 18:48:48 +0800

    fix average time

--- src/main/java/com/test/performance/progress/ShowProgressImpl.java
+++ src/main/java/com/test/performance/progress/ShowProgressImpl.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class ShowProgressImpl implements ShowProgressable {
 	
-	private static final String REPORT_FORMAT = "[Report] send total requests [%s] (fail requests: [%s]) with TPS [%.1f] (average response time: [%s]mills) comsume [%s]seconds";
+	private static final String REPORT_FORMAT = "[Report] send total requests [%s] (fail requests: [%s]) with TPS [%.1f] (average response time in report interval: [%.1f]mills) comsume [%s]seconds";
 
 	private ScheduledExecutorService scheduledExecutorService;
 	private int reportProgressIntervalInSeconds;
@@ -16,7 +16,9 @@ public class ShowProgressImpl implements ShowProgressable {
 
 	protected AtomicLong totalRequests = new AtomicLong();
 	protected AtomicLong failRequests = new AtomicLong();
-	protected AtomicLong totalRequestsComsumeTime = new AtomicLong();
+	
+	protected AtomicLong totalRequestsInReportInterval = new AtomicLong();
+	protected AtomicLong totalRequestsComsumeTimeInReportInterval = new AtomicLong();
  	
 	public ShowProgressImpl(int reportProgressIntervalInSeconds) {
 		this.reportProgressIntervalInSeconds = reportProgressIntervalInSeconds;
@@ -29,15 +31,32 @@ public class ShowProgressImpl implements ShowProgressable {
 		public void run() {
 			synchronized (ShowProgressImpl.class) {
 				long duration = System.currentTimeMillis() - startTime;
-				System.out.println(String.format(REPORT_FORMAT, totalRequests.get(), failRequests.get(), totalRequests.get() * 1000d/duration, 0, duration/1000));
+				System.out.println(String.format(REPORT_FORMAT, totalRequests.get(), failRequests.get(), totalRequests.get() * 1000d/duration, getAverageResponseTimeInReportInterval(), duration/1000));
+				resetIntervalData();
 			}
+		}
+
+		private void resetIntervalData() {
+			totalRequestsInReportInterval.set(0);
+			totalRequestsComsumeTimeInReportInterval.set(0);
+		}
+		
+		private double getAverageResponseTimeInReportInterval(){
+			long totalRequestsInReportIntervalValue = totalRequestsInReportInterval.get();
+			if(totalRequestsInReportIntervalValue == 0){
+				return -1;
 			}
+			return Double.valueOf(totalRequestsComsumeTimeInReportInterval.get())/totalRequestsInReportIntervalValue;
+			
+		}
 	}
 	
 	@Override
 	public void record(boolean isSuccess, long duration){
 		synchronized (ShowProgressImpl.class) {
 			totalRequests.incrementAndGet();
+			totalRequestsInReportInterval.incrementAndGet();
+			totalRequestsComsumeTimeInReportInterval.getAndAdd(duration);
 			if(!isSuccess){
 				failRequests.incrementAndGet();
 			}
